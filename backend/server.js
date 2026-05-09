31977b41332cc5cb5021af1a28639a4f2d3f9981
Sugandhi Sahni <sugandhisahni@Sugandhis-MacBook-Air-4.local>
2026-05-09 11:56:29 +0530

    Fix Express 5 CORS preflight route

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
